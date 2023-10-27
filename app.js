ede0e431c032fe71a3f470f79ac4edd88905971d
Naveenreddy70 <142835943+Naveenreddy70@users.noreply.github.com>
2023-10-26 19:51:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var mydataRouter = require('./routes/mydata');
 var computationRouter = require('./routes/computation');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -24,8 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
-app.use('/computation', computationRouter);
-
+app.use('/computation',computationRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
